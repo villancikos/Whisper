6327bfca9cc9547c4ca00ed486a8bb6477396556
villancikos <villancikos@gmail.com>
2017-02-27 17:18:16 +0000

    Simplify code with fat arrow ES6

--- WebClient/scripts/main.js
+++ WebClient/scripts/main.js
@@ -28,94 +28,6 @@ const user_id = 'jurdini01';
 const usersRef = database.ref("users/" + user_id);
 const participantsRef = database.ref("participants/");
 
-
-
-
-
-// console.log(usersRef.users.ramiri01.conversations);
-// Get all the conversations to which user has access
-Object.keys(usersRef).map(function (key) {
-    // console.log(usersRef[key]);
-    // console.log(usersRef[key]);
-});
-
-usersRef.once('value', snapshot => {
-    // Obtain the conversations of this user
-    var conversations = Object.keys(snapshot.child("/conversations/").val());
-    for (var i = 0; i < conversations.length; i++) {
-        database.ref('/conversations/' + conversations[i]).once('value', childSnapshot => {
-            // console.log(childSnapshot.val());
-
-        });
-    }
-});
-
-// console.log(conversationList);
-
-usersRef.once('value', snapshot => {
-    var data = snapshot.val()
-    var keys = Object.keys(data)
-    // console.log(keys);
-});
-
-// Now we have the conversations which this user should access.
-// Let's fetch them!
-// convosRef.once('value', function(snapshot){
-
-// });
-
-
-// console.log(database.ref('/participants/').equalTo('jurdini01').val());
-// database.ref('/participants/').orderByChild('jurdini01').startAt(!null).once('value').then(function (snapshot) {
-//     console.log(snapshot.val());
-// });
-
-
-participantsRef.once('value', snapshot => {
-    var data = snapshot.val()
-    var keys = Object.keys(data)
-    // console.log(data);
-});
-
-// database.ref('/conversations/').once('value')
-//     .then(function (snapshot) {
-//         snapshot.forEach(function (childSnapshot) {
-//             console.log(childSnapshot.val());
-//         })
-//     })
-
-// var test = function () {
-// Object.keys(conversationList).map(function (key) {
-// debugger;
-// console.log(conversationList[key]);
-// convosRef.on('value', snapshot =>{
-//     if (snapshot.val() !== key){
-//         console.log(snapshot.val());
-//     }
-// });
-// })
-// };
-
-
-
-// const conversationsReference = database.ref("users/"+ user_id+"/conversations/");
-// conversationsReference.once("value")
-//   .then(function(snapshot) {
-//     snapshot.forEach(function(childSnapshot) {
-//       // key will be "ada" the first time and "alan" the second time
-//       database.ref("/conversations/"+childSnapshot.key).on("value")
-//       .then(function(othersnapshot){
-//         console.log(othersnapshot.val());
-//       })
-//     //   var key = childSnapshot.key;
-//     //   console.log(key);
-//       // childData will be the actual contents of the child
-//     //   var childData = childSnapshot.val();
-//     //   console.log(childData);
-//   });
-// });
-
-
 /*
     Main Wrapper for our Whisper Web App
 */
@@ -128,62 +40,17 @@ var AppWrapper = React.createClass({
         }
     },
     componentDidMount: function () {
-        // usersRef.once('value', snapshot => {
-        //     // Obtain the conversations of this user
-        //     var conversations = Object.keys(snapshot.child("/conversations/").val());
-        //     var test = {};
-        //     console.log(conversations);
-        //     console.log(snapshot.val());
-        //     for (var conv_key in conversations) {
-        //         console.log(conversations[conv_key]);
-
-        //         database.ref('/conversations/'+conversations[conv_key]).once('value')
-        //         .then(function(childSnapshot){
-        //             console.log(childSnapshot.val());
-        //             // var data = childSnapshot.val();
-
-        //             // var key = Object.keys(data);
-        //             // this.setState({conversations: childSnapshot.val()});
-        //             // this.state.conversations[key]=data;
-        //         });
-        //     }
-        //     // this.setState({ conversations: this.state.conversations});
-        // });
-        // usersRef.on('value', snap => snap.forEach((subSnap) => console.log(subSnap.val())));
-
-        var myObject = {};
-        usersRef.on('value', function (userSnapshot) {
-
-            // First we get the qty of conversations.
-            var conversationCount = userSnapshot.child('conversations').numChildren();
-            // we initialize an empty counter
-            var conversationLoadedCount = 0;
-            // we traverse now the conversations ref with the past conversations.
-            userSnapshot.child('conversations').forEach(function (conversationKey) {
+        usersRef.on('value', userSnapshot => {
+            userSnapshot.child('conversations').forEach(conversationKey => {
                 var conversationRef = database.ref('conversations').child(conversationKey.key);
-                conversationRef.on('value', function (conversationsSnapshot) {
+                conversationRef.on('value', conversationsSnapshot => {
                     var conversation = conversationsSnapshot.val();
-
-                    conversationLoadedCount = conversationLoadedCount + 1;
-                    myObject[conversationKey.key] = conversation;
-                    // console.log(conversationKey.key);
                     this.state.conversations[conversationKey.key] = conversation;
                     this.setState({ conversations: this.state.conversations });
-                    if (conversationLoadedCount === conversationCount) {
-                        console.log("We've loaded all conversations");
-                    }
-                }.bind(this));
-            }.bind(this));
-        }.bind(this));
-        // this.setState({ conversations: this.state.conversations });
-        // this.setState({ conversations: this.state.conversations })
-        // this.setState({conversations: this.state.conversations});
-        // Load all Sample Conversations
-        // convosRef.on('value', snapshot => {
-        //     this.setState({ conversations: snapshot.val() });
-        // });
-        // debugger;
-        // this.setState({conversations: myObject});
+                });
+            });
+        });
+
         this.setState({
             participants: {
                 conversation0001: {
@@ -237,15 +104,17 @@ var AppWrapper = React.createClass({
         database.ref().update(updates);
 
     },
-    actAsUser: function(){
-
+    actAsUser: function (event) {
+        event.preventDefault();
+        this.refs.user.value
+        this.setState({ user: user })
     },
     render: function () {
         return (
             <div className="row">
-            <form onSubmit={this.actAsUser}>
-            <input type="text" ref="useridref"/>
-            </form>
+                <form onSubmit={this.actAsUser}>
+                    <input type="text" ref="useridref" />
+                </form>
                 <ConversationsSideBar conversations={this.state.conversations} refreshConversationPanel={this.refreshConversationPanel} />
                 <ConversationPanel availableConversations={this.state.availableConversations} addNewMessage={this.addNewMessage} />
             </div>
