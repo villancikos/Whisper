6d16d729c61f68c46826ecf1b51998898b0b7eec
villancikos <villancikos@gmail.com>
2017-03-17 18:08:19 +0000

    addedd redux-thunk for async calls

--- WebClient/client/store.js
+++ WebClient/client/store.js
@@ -1,7 +1,7 @@
-import { createStore, compose } from 'redux';
+import { createStore,applyMiddleware } from 'redux';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router';
-
+import thunkMiddleware from 'redux-thunk'
 // Sample Data
 import users from './sample-data/sample-users';
 import conversations from './sample-data/sample-conversations';
@@ -26,7 +26,12 @@ const defaultState = {
 }
 
 
-const store = createStore(rootReducer, defaultState);
+const store = createStore(
+    rootReducer, 
+    defaultState,
+    applyMiddleware(
+        thunkMiddleware // let us use "not pure" funtions on dispatch
+    ));
 
 export const history = syncHistoryWithStore(browserHistory, store);
 export default store;
