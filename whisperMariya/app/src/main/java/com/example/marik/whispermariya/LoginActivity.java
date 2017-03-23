229cb96680f537248fc022fd5eb1b27c57af15d8
Mariya A <abmariya1991@gmail.com>
2017-03-23 19:04:40 +0000

    autorization problem solution

---
+++ whisperMariya/app/src/main/java/com/example/marik/whispermariya/LoginActivity.java
@@ -0,0 +1,105 @@
+package com.example.marik.whispermariya;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity{
+
+    private Button btnSingIn;
+    private EditText username;
+    private EditText password;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        btnSingIn = (Button) findViewById(R.id.btnSignIn);
+        username = (EditText) findViewById(R.id.txt_username);
+        password = (EditText) findViewById(R.id.txt_password);
+
+        final Intent i = new Intent(LoginActivity.this, MainActivity.class);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d("AAAAAAAAAAAAAAAAAA", "onAuthStateChanged:signed_in:" + user.getUid());
+                    i.putExtra("username", user.getEmail());
+                    startActivity(i);
+                } else {
+                    // User is signed out
+                    Log.d("AAAAAAAAAAAAAAAAAA", "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if(auth.getCurrentUser() != null) {
+            Toast.makeText(LoginActivity.this, "cheers Found an user", Toast.LENGTH_LONG).show();
+            i.putExtra("username", auth.getCurrentUser().getEmail());
+            startActivity(i);
+        }
+
+        btnSingIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    mAuth.signInWithEmailAndPassword(username.getText().toString(), password.getText().toString())
+                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    Log.d("AAAAAAAAAAAAAAAAAA", "signInWithEmail:onComplete:" + task.isSuccessful());
+                                    if (!task.isSuccessful()) {
+                                        Log.w("AAAAAAAAAAAAAAAAAA", "signInWithEmail:failed", task.getException());
+                                        Toast.makeText(LoginActivity.this, "EROROOROOROROROROROR",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+
+                                }
+                            });
+            }
+        });
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+}
--- whisperMariya/app/src/main/java/com/example/marik/whispermariya/MainActivity.java
+++ whisperMariya/app/src/main/java/com/example/marik/whispermariya/MainActivity.java
@@ -2,8 +2,6 @@ package com.example.marik.whispermariya;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,10 +14,6 @@ import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.database.FirebaseListAdapter;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -34,18 +28,17 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     private Button add_room;
     private EditText room_name;
-    private static int SIGN_IN_REQUEST_CODE = 1;
     RelativeLayout activity_main;
     private String name;
 
 
     private ListView listView;
     private ArrayAdapter<String> arrayAdapter;
-    private FirebaseListAdapter<Message> messageAdapter;
+
     private ArrayList<String> list_of_rooms = new ArrayList<>();
     private DatabaseReference root = FirebaseDatabase.getInstance().getReference().getRoot();
 
@@ -60,60 +53,43 @@ public class MainActivity extends AppCompatActivity {
         room_name = (EditText) findViewById(R.id.tvChatName);
         listView = (ListView) findViewById(R.id.lvChatList);
 
-
-        //request_user_name();
+        name = getIntent().getStringExtra("username");
 
         add_room.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 EditText input = (EditText) findViewById(R.id.tvChatName);
-                FirebaseDatabase.getInstance().getReference().push()
-                        .setValue(new Message(input.getText().toString(),
-                                FirebaseAuth.getInstance().getCurrentUser().getEmail()));
+                FirebaseDatabase.getInstance().getReference().push().setValue(new Message(input.getText().toString(), name));
                 input.setText("");
             }
         });
 
-        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
-            startActivityForResult(AuthUI.getInstance()
-                    .createSignInIntentBuilder()
-                    .build(), SIGN_IN_REQUEST_CODE);
-        } else {
-
-            displayChats();
-        }
-
+        displayChats();
 
         arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list_of_rooms);
         listView.setAdapter(arrayAdapter);
 
         add_room.setOnClickListener(new View.OnClickListener() {
-
             @Override
-            public void onClick(View view) {
-
+            public void onClick(View v) {
                 Map<String, Object> map = new HashMap<String, Object>();
-                map.put(room_name.getText().toString(), "");
+                map.put(room_name.getText().toString(),"");
                 root.updateChildren(map);
-
             }
-
         });
 
         root.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-
                 Set<String> set = new HashSet<String>();
                 Iterator i = dataSnapshot.getChildren().iterator();
 
-                while (i.hasNext()) {
-                    set.add(((DataSnapshot) i.next()).getKey());
+                while(i.hasNext()){
+                    set.add(((DataSnapshot)i.next()).getKey());
                 }
 
                 list_of_rooms.clear();
                 list_of_rooms.addAll(set);
-
                 arrayAdapter.notifyDataSetChanged();
             }
 
@@ -121,20 +97,18 @@ public class MainActivity extends AppCompatActivity {
             public void onCancelled(DatabaseError databaseError) {
 
             }
-
-
         });
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l){
-
-                Intent intent = new Intent(getApplicationContext(),Chat_room.class);
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), Chat_room.class);
                 intent.putExtra("room_name",((TextView)view).getText().toString());
                 intent.putExtra("user_name",name);
                 startActivity(intent);
             }
         });
+
 
     }
 
@@ -174,22 +148,6 @@ public class MainActivity extends AppCompatActivity {
         */
     }
 
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, requestCode, data);
-        if (requestCode == SIGN_IN_REQUEST_CODE) {
-            if (requestCode == RESULT_OK) {
-                Snackbar.make(activity_main, "Signed in", Snackbar.LENGTH_SHORT).show();
-                displayChats();
-
-            } else {
-                Snackbar.make(activity_main, "You didn't sign in.Please try again", Snackbar.LENGTH_SHORT).show();
-                finish();
-            }
-        }
-    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
@@ -199,16 +157,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.menu_signout) {
-            AuthUI.getInstance().signOut(this)
-                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-
-                            Snackbar.make(activity_main, "Signed out", Snackbar.LENGTH_SHORT).show();
-                            finish();
-
-                        }
-                    });
+            FirebaseAuth.getInstance().signOut();
+            Intent i = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(i);
+            finish();
         }
         return true;
     }
