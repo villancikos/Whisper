118594f2012f39f309ba5feee5d675295397f0f6
Santiago Arrubla <sl.arrubla77@gmail.com>
2017-03-27 06:47:36 -0500

    styles modification

--- WebClient/client/components/Message.js
+++ WebClient/client/components/Message.js
@@ -15,26 +15,24 @@ export default class Message extends React.Component {
                 <div className="speech-whispper">
                     <div className="bubble">
                         <div className="bubble-txt">
-                            {this.props.loggedUser === message_data.sender ?
-                            <p className="bubble-name sender">{message_data.sender}</p>
-                            :
-                            <p className="bubble-name">{message_data.sender}</p>
-                            }
+
                             <p className="bubble-message">{message_data.content}</p>
                             <span className="bubble-timestamp">
                             {message_data.timestamp !== '' 
                             ?
                             <i className="fa fa-clock-o mr-1"></i>
                             :'' }
-                            {message_data.timestamp}
+                            {
+                                message_data.timestamp}
                             </span>
                         </div>
-                    </div>
+                    
                     {this.props.loggedUser === message_data.sender
                     ?
                         <div className="bubble-arrow sender"></div>
                     :
-                        <div className="bubble-arrow"></div>}
+                        <div className="bubble-arrow "></div>}
+                </div>
                 </div>
         )
     }
@@ -42,32 +40,13 @@ export default class Message extends React.Component {
 
 }
 
-// CODIGO PARA BURBUJA ALTERNATIVA...
-/*
-  <div className="bubble-sender-receiver">
-    <div className="bubble-receiver-txt">
-      <p className="bubble-receiver-name">+353 87 1234 567<span> ~ John</span></p>
-      <p className="bubble-receiver-message">Nice... this will work great for my new project.</p>
-      <span className="bubble-receiver-timestamp">10:22 pm</span>
-    </div>
-    <div className="bubble-receiver-arrow alt"></div>
-  </div>
+
+/* Coigo para que salga el nombre norrado
+
+                           {this.props.loggedUser === message_data.sender ?
+                            
+                            <p className="bubble-name sender">{message_data.sender}</p>
+                            :
+                            <p className="bubble-name">{message_data.sender}</p>
+                            }
 */
-
-
-
-
-// CODIGO FERNANDO ORIGINAL
-/*
-            <div className="p-0" style={{ border: "solid 0.1px gray" }}>
-                <div className="message-sender">
-                    {message_data.sender}</div>
-                <p>{message_data.content}</p>
-                <div className="message-timestamp">
-                    <i className="fa fa-clock-o mr-1"></i>
-                    {message_data.timestamp}
-                </div>
-            </div>
-*/
-
-
